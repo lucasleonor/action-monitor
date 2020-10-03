86f248eddc7ea516c98788ae8cdfad316441dc3a
Lucas Leonor <lucas.g.leonor@gmail.com>
2020-10-03 16:16:23 -0300

    Initial commit

---
+++ src/test/java/com/betvictor/test/messaging/actionmonitor/ActionMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betvictor.test.messaging.actionmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
